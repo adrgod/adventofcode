3395251e83b48290160470f5916438a7b135cec8
Adriano Godinho <adriano.daniel.goncalves.godinho@verizonconnect.com>
2025-10-10 09:37:56 +0100

    feat: added findall to search word

--- 2024/04/202404.py
+++ 2024/04/202404.py
@@ -1,6 +1,7 @@
 import os
+import re
 
-f = open('test_input.txt', 'r')
+f = open('input04.txt', 'r')
 data = f.read()
 
 find_word = 'XMAS'
@@ -9,8 +10,7 @@ def find_word_horizontally(data, find_word):
     lines = data.split('\n')
     number_finds = 0
     for line in lines:
-        if find_word in line:
-            number_finds += 1
+        number_finds += re.findall(find_word, line).__len__()
     return number_finds
 
 def find_word_vertically(data, find_word):
@@ -19,8 +19,7 @@ def find_word_vertically(data, find_word):
 
     for i in range(len(lines[0])):
         column = ''.join([line[i] for line in lines])
-        if find_word in column:
-            number_finds += 1
+        number_finds += re.findall(find_word, column).__len__()
     return number_finds
 
 def find_word_diagonally(data, find_word):
@@ -30,8 +29,7 @@ def find_word_diagonally(data, find_word):
         for j in range(len(lines[i])):
             if i + len(find_word) <= len(lines) and j + len(find_word) <= len(lines[i]):
                 diagonal = ''.join([lines[i + k][j + k] for k in range(len(find_word))])
-                if find_word in diagonal:
-                    number_finds += 1
+                number_finds += re.findall(find_word, diagonal).__len__()
     return number_finds
 
 def find_word_diagonally_reverse(data, find_word):
@@ -41,8 +39,7 @@ def find_word_diagonally_reverse(data, find_word):
         for j in range(len(lines[i])):
             if i + len(find_word) <= len(lines) and j + len(find_word) <= len(lines[i]):
                 diagonal = ''.join([lines[i + k][j + k] for k in range(len(find_word))])
-                if find_word in diagonal:
-                    number_finds += 1
+                number_finds += re.findall(find_word, diagonal).__len__()
     return number_finds
 
 def find_word_in_grid(data, find_word):
